0cc75c44e1c55ffbb57459a6ff38a0923d31c340
mikefratelli <42177196+mikefratelli@users.noreply.github.com>
2019-02-08 07:43:11 -0800

    Create kill_idlelogins.sh

---
+++ kill_idlelogins.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#read /usr/local/etc/group/all, per each line..
+while read line
+  do
+#giving hostnames their own variable (might prevent race conditions?)
+  HOST=`echo "$line" `
+# .. per each line find out if I’m logged on, and for how long I'm idle in seconds..
+  SEC=`timeout 2s dsh -m -g $HOST w mmorgan |awk '{
+# translate days, minutes, hours, etc into seconds
+if (NR!=1){
+if($5 ~ /days/){
+    split($5,d,"days");
+    print $1,d[1]*5184000" sec"
+}
+else if( $5 ~ /:|s/){
+    if ($5 ~/s/) { sub(/s/,"",$5); split($5,s,"."); print $1,s[1]" sec" }
+    else if ( $5 ~/m/) { split($5,m,":"); print $1,(m[1]*60+m[2])*60" sec" }
+    else { split($5,m,":"); print $1,m[1]*60+m[2]" sec" }
+}
+else { print $1,$5}
+}}'| awk '{print $2}' | tail -n +2`
+  echo "idle time is $SEC"
+    #is it greater than an hour?
+   if [ "$SEC " -ge "3600" ]
+    #If so, then kill all my processes, so I’m logged off
+    then
+    echo "killing all mmorgan's processes on $HOST"
+    dsh -m -g "$HOST" pkill -KILL -u 8671
+    fi
+  done </usr/local/etc/group/all
