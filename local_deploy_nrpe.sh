e1da661607b10f0450361cf2c9e49e1d10949a27
mikefratelli <42177196+mikefratelli@users.noreply.github.com>
2019-02-22 13:34:06 -0800

    formatting

--- local_deploy_nrpe.sh
+++ local_deploy_nrpe.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #This script will install nrpe on a remote server for you
 yum groupinstall -y "Development tools"
-if [ -z  "`ls /home/$USER/downloads 2>/dev/null`"]; then
-mkdir /home/$USER/downloads
-fi
+  if [ -z  "`ls /home/$USER/downloads 2>/dev/null`"]; then
+    mkdir /home/$USER/downloads
+  fi
 cd /home/$USER/downloads && wget https://nagios-plugins.org/download/nagios-plugins-2.2.1.tar.gz#_ga=2.78936084.1450957153.1550765996-1666183519.1550251195
 tar xzf /home/$USER/downloads/nagios-*
 cd /home/$USER/downloads/nagios-* && /home/$USER/downloads/nagios-*/configure && make && make install
-if [ -z "`getent passwd nagios`" ]; then
-useradd nagios && groupadd nagios && usermod -a -G nagios nagios && chown nagios.nagios /usr/local/nagios && chown -R nagios.nagios /usr/local/nagios/libexec
-else
-echo "user nagios already exists"
-fi
- yum install -y xinetd
- cd /home/$USER/downloads && wget https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-3.2.1/nrpe-3.2.1.tar.gz && tar zxf nrpe-3.2.1.tar.gz
- cd /home/$USER/downloads/nrpe-3.2.1 && /home/$USER/downloads/nrpe-3.2.1/configure && make all && make install-groups-users && make install && make install-config && make install-inetd
-if [ -z "` cat /etc/services | grep nrpe`" ]; then
- echo "nrpe     5666/tcp" >> /etc/services
-fi
- cat > /etc/xinetd.d/nrpe << EOL
- service nrpe
+  if [ -z "`getent passwd nagios`" ]; then
+    useradd nagios && groupadd nagios && usermod -a -G nagios nagios && chown nagios.nagios /usr/local/nagios && chown -R nagios.nagios /usr/local/nagios/libexec
+  else
+    echo "user nagios already exists"
+  fi
+yum install -y xinetd
+cd /home/$USER/downloads && wget https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-3.2.1/nrpe-3.2.1.tar.gz && tar zxf nrpe-3.2.1.tar.gz
+cd /home/$USER/downloads/nrpe-3.2.1 && /home/$USER/downloads/nrpe-3.2.1/configure && make all && make install-groups-users && make install && make install-config && make install-inetd
+  if [ -z "` cat /etc/services | grep nrpe`" ]; then
+    echo "nrpe     5666/tcp" >> /etc/services
+  fi
+cat > /etc/xinetd.d/nrpe << EOL
+service nrpe
 {
     disable         = no
     socket_type     = stream
@@ -33,7 +33,7 @@ fi
     log_on_success  =
 }
 EOL
- systemctl restart xinetd.service
- netstat -at | egrep "nrpe|5666"
- /usr/local/nagios/libexec/check_nrpe -H localhost
- /usr/local/nagios/libexec/check_nrpe -H localhost -c check_users
+systemctl restart xinetd.service
+netstat -at | egrep "nrpe|5666"
+/usr/local/nagios/libexec/check_nrpe -H localhost
+/usr/local/nagios/libexec/check_nrpe -H localhost -c check_users
